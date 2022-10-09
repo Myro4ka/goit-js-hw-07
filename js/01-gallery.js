4e05cf2822937244ca07ac1bc919a52095a69a1b
Myroslava Siryk <www.mirosya@gmail.com>
2022-10-09 23:24:39 +0300

    Create and render markup to 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryMarkup);
+// console.log(createGalleryItemsMarkup(galleryItems));
+// console.log(galleryContainer);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+    class="gallery__image"
+    src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+    </a>
+    </div>
+    `;
+    })
+    .join("");
+}
